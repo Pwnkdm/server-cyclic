84154bd37be6c9b79aa651475c31f795b07786ca
Pwnkdm <pawanrajkadam@gmail.com>
2022-12-27 18:37:34 +0530

    cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import userController from "./Routes/userRoute.js";
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:8080",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
