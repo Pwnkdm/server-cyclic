2b71c8336f644cd55557dee407dc118bbd19df24
Pawanraje Kadam <99590191+Pwnkdm@users.noreply.github.com>
2023-01-16 13:30:44 +0530

    delete

--- Config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-mongoose.set("strictQuery", false);
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-export default mongoose.connect(process.env.MONGOURL);
